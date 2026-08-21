aa45ecc8efee4928890371e9df50b75a1ac33a71
Shrinivas <shrinivas.atre@gmail.com>
2026-08-21 22:24:03 +0530

    Deploy pinned Face Landmarker model

---
+++ mediapipe/FaceLandmarkerModel.cmake
@@ -0,0 +1,5 @@
+# Pinned runtime model contract used by the application CMake build.
+set(FACE_MEDIAPIPE_MODEL_FILENAME "face_landmarker.task")
+set(FACE_MEDIAPIPE_MODEL_SHA256
+    "64184e229b263107bc2b804c6625db1341ff2bb731874b0bcc2fe6544e0bc9ff"
+)
